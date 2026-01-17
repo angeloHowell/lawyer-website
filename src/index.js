62cae60dfb83c93d15726bdd0f9a3005a2257c74
angeloHowell <ahowell@utb.edu.co>
2026-01-17 13:23:22 -0500

    big changes on index.js

--- src/index.js
+++ src/index.js
@@ -10,17 +10,7 @@ import "bootstrap/dist/js/bootstrap.min.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
