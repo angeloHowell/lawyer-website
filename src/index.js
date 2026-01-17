0ac2005f430553560befa099eb8eb426792531af
angeloHowell <ahowell@utb.edu.co>
2026-01-17 11:44:47 -0500

    new port line added in client/src/index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import "bootstrap/dist/js/bootstrap.min.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-
+const port = 3000;
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
