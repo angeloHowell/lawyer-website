210dacd74803ad4b5327d2af2d36173fb1637a74
angeloHowell <ahowell@utb.edu.co>
2026-06-28 16:07:31 -0500

    chore: sitemap.xml and ErrorPage.js updates

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,6 +1,15 @@
+import { Helmet } from "react-helmet";
+
 const ErrorPage = () => {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center text-center vh-100">
+      <Helmet>
+        <title>Página no encontrada | Howell & Abogados</title>
+        <meta
+          name="description"
+          content="La página que busca no está disponible. Howell & Abogados - Asesoría Legal Integral en Colombia."
+        />
+      </Helmet>
       <h4>Error 404</h4>
       <p>No se ha encontrado la página.</p>
     </div>
